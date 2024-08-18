0a1e65d5627e5331013921a7fc2e72e19cea39a8
vaishnavi-kalaskar <vaishnavikalskar02@gmail.com>
2024-08-18 21:07:08 +0530

    Add files via upload

---
+++ 8-aug-2024.js.js
@@ -0,0 +1,60 @@
+//  var a;
+//  a=8;
+
+// console.log(a);
+// console.log(typeof a);
+
+// var b='hii'
+// console.log(b);
+// console.log(typeof b);
+
+// var a='bye'
+// console.log(a)
+// console.log(typeof a)
+
+// let x=55
+// console.log(x)
+// console.log(typeof x)
+// // let x
+
+// const n=2.17
+// console.log(n)
+// console.log(typeof n)
+
+// n='234'
+
+
+// scope of variable
+
+// var x=10;
+// console.log(x);
+// {
+//     var x=34
+//     console.log(x)
+// }
+// console.log(x)
+
+
+let y=14
+console.log(y)
+{
+    let y=1
+    console.log(y)
+    y="bappa"
+    console.log(y)
+}
+console.log(y)
+
+
+const z=14
+console.log(z)
+{
+    // const y=1
+    // TypeError: Assignment to constant variable.
+    console.log(z)
+//    var z="bappa"
+//    SyntaxError: Identifier 'z' has already been declared (at script.js:55:8)
+    console.log(z)
+}
+console.log(z)
+
