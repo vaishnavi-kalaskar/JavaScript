dbfa05a01e467343eec6959da930320fc7164b6b
vaishnavi-kalaskar <vaishnavikalskar02@gmail.com>
2024-09-11 14:19:54 +0530

    Add files via upload

---
+++ 5-sep-2024.js
@@ -0,0 +1,77 @@
+//window obj
+// let b = document.getElementById("btn");
+// console.log(b);
+
+// b.addEventListener("click", function () {
+//   //   window.open("https://www.apple.com/in", "_blank","width=200","height=150","top=200","left=400");
+//   console.log(window.innerHeight);
+//   console.log(window.innerWidth);
+//   console.log(window.outerHeight);
+//   console.log(window.outerWidth);
+// });
+
+//property call function
+
+//location object
+// let c = document.getElementById("btn");
+// console.log(c);
+
+// c.addEventListener("click", function () {
+// //   location.href = "https://www.example.com";
+//   //   location.assign = "https://www.example.com";
+//   //   location.replace = "https://www.example.com";
+//   location.reload();
+// });
+
+//navigator object
+// let c = document.getElementById("btn");
+// console.log(c);
+// c.addEventListener("click", function () {
+//   console.log(navigator.userAgent);
+//   console.log(navigator.cookieEnabled);
+// });
+
+//screen object
+let c = document.getElementById("btn");
+console.log(c);
+c.addEventListener("click", function () {
+  console.log(screen);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//export let a=50
+
+// import {a} from ./file name
+//call
